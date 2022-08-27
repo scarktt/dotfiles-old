3e396bd711d718ab644d52a96323f1f59e351e3b
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-26 21:01:21 -0600

    Add first nvim lua config

---
+++ nvim-lua/init.lua
@@ -0,0 +1,60 @@
+
+require('globals.options')
+require('globals.remaps')
+
+require('plugins')
+require('settings')
+
+require'nvim-treesitter.configs'.setup {
+	ensure_installed = { "python", "javascript", "typescript", "vue", "css", "html", "json", "markdown", "regex", "vim", "yaml" },
+	sync_install = false,
+  	auto_install = true,
+	highlight = {
+		enable = true,
+		--disable = { "lua" },
+	},
+}
+
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+		theme = 'onedark',
+		-- theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {'filename'},
+    lualine_x = {'location'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  extensions = {}
+}
+
+
+vim.opt.termguicolors = true
+
+vim.opt.list = true
+vim.opt.listchars:append("space:⋅")
+vim.opt.listchars:append("eol:↴")
+
+require("indent_blankline").setup {
+    space_char_blankline = " ",
+    show_current_context = true,
+    show_current_context_start = true,
+}
