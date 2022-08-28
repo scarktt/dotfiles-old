0a81efe515dbf25292acb902c27c7c5e0b3436f8
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-27 23:18:50 -0600

    Add buffers movement keybinding

--- nvim-lua/lua/globals/remaps.lua
+++ nvim-lua/lua/globals/remaps.lua
@@ -1,33 +1,38 @@
 
 vim.g.mapleader = ' '
 
+local options = { noremap = true, silent = true }
 local os_name = os.execute('uname -a')
 
---vim.api.nvim_set_keymap('n', '<leader>1', '')
-
-vim.keymap.set('n', '<leader>fs', "<cmd>w<cr>", { silent = true})
-vim.keymap.set('n', '<leader>q', "<cmd>q<cr>", { silent = true})
-vim.keymap.set('n', '<leader>qq', "<cmd>q!<cr>", { silent = true})
-vim.keymap.set('n', '<leader>wh', "<cmd>split<cr>", { silent = true})
-vim.keymap.set('n', '<leader>wv', "<cmd>vsplit<cr>", { silent = true})
-vim.keymap.set('n', '<leader>th', "<cmd>split<cr> <cmd>te pwsh.exe<cr>", { silent = true})
-vim.keymap.set('n', '<leader>tv', "<cmd>vsplit<cr> <cmd>te pwsh.exe<cr>", { silent = true})
+vim.keymap.set('n', '<leader>fs', "<cmd>w<cr>", options)
+vim.keymap.set('n', '<leader>q', "<cmd>q<cr>", options)
+vim.keymap.set('n', '<leader>qq', "<cmd>q!<cr>", options)
+vim.keymap.set('n', '<leader>wh', "<cmd>split<cr>", options)
+vim.keymap.set('n', '<leader>wv', "<cmd>vsplit<cr>", options)
+vim.keymap.set('n', '<leader>th', "<cmd>split<cr> <cmd>te pwsh.exe<cr>", options)
+vim.keymap.set('n', '<leader>tv', "<cmd>vsplit<cr> <cmd>te pwsh.exe<cr>", options)
 
 -- reload config
-vim.keymap.set('n', '<leader>rr', "<cmd>so %<cr>", { silent = true})
+vim.keymap.set('n', '<leader>rr', "<cmd>so %<cr>", options)
 
--- deselect macthing string after search
-vim.keymap.set('n', '<leader>.', "<cmd>noh<cr>", { silent = true})
+-- deselect matching string after search
+vim.keymap.set('n', '<leader>.', "<cmd>noh<cr>", options)
 
-vim.keymap.set('n', '<leader>1', "<cmd>BufferPrevious<cr> <cmd>cd %:h<cr>", { silent = true})
-vim.keymap.set('n', '<leader>bd', "<cmd>:BufferClose!<cr>", { silent = true})
+-- barbar buffers
+vim.keymap.set('n', '<leader>1', "<cmd>BufferPrevious<cr> <cmd>cd %:h<cr>", options)
+vim.keymap.set('n', '<leader>2', "<cmd>BufferNext<cr> <cmd>cd %:h<cr>", options)
+vim.keymap.set('n', '<leader>bd', "<cmd>BufferClose!<cr>", options)
+vim.keymap.set('n', '<leader>bp', "<cmd>BufferPin<cr>", options)
+vim.keymap.set('n', '<leader><', "<cmd>BufferMovePrevious<cr>", options)
+vim.keymap.set('n', '<leader>>', "<cmd>BufferMoveNext<cr>", options)
 
+vim.keymap.set('n', '<leader>n', "<CMD>NERDTree<CR>'", options)
 
 -- files
 -- open File Explorer in the current dir
 if string.find(os_name, "linux") then
-    vim.keymap.set('n', '<leader>f.', "<cmd>!open .<cr>", { silent = true})
+    vim.keymap.set('n', '<leader>f.', "<cmd>!open .<cr>", options)
 else
     -- windows path
-    vim.keymap.set('n', '<leader>f.', "<cmd>!start explorer /select,%:p<cr>", { silent = true})
+    vim.keymap.set('n', '<leader>f.', "<cmd>!start explorer /select,%:p<cr>", options)
 end
