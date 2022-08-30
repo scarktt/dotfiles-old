70dbbb03ab038c7ea87f883563cdb84b1e31b168
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-30 14:03:43 -0600

    Add windows navigation keybinding

--- nvim-lua/lua/globals/remaps.lua
+++ nvim-lua/lua/globals/remaps.lua
@@ -4,16 +4,25 @@ vim.g.mapleader = ' '
 local options = { noremap = true, silent = true }
 local os_name = os.execute('uname -a')
 
-vim.keymap.set('n', '<leader>fs', "<cmd>w<cr>", options)
-vim.keymap.set('n', '<leader>q', "<cmd>q<cr>", options)
-vim.keymap.set('n', '<leader>qq', "<cmd>q!<cr>", options)
+-- navigate windows
+vim.keymap.set('n', '<c-k>', "<cmd>wincmd k<cr>", options)
+vim.keymap.set('n', '<c-j>', "<cmd>wincmd j<cr>", options)
+vim.keymap.set('n', '<c-h>', "<cmd>wincmd h<cr>", options)
+vim.keymap.set('n', '<c-l>', "<cmd>wincmd l<cr>", options)
+
+-- split windows
 vim.keymap.set('n', '<leader>wh', "<cmd>split<cr>", options)
 vim.keymap.set('n', '<leader>wv', "<cmd>vsplit<cr>", options)
 vim.keymap.set('n', '<leader>th', "<cmd>split<cr> <cmd>te pwsh.exe<cr>", options)
 vim.keymap.set('n', '<leader>tv', "<cmd>vsplit<cr> <cmd>te pwsh.exe<cr>", options)
 
+-- save and quite
+vim.keymap.set('n', '<leader>fs', "<cmd>w<cr>", options)
+vim.keymap.set('n', '<leader>q', "<cmd>q<cr>", options)
+vim.keymap.set('n', '<leader>qq', "<cmd>q!<cr>", options)
+
 -- reload config
-vim.keymap.set('n', '<leader>rr', "<cmd>so %<cr>", options)
+vim.keymap.set('n', '<leader>rr', "<cmd>luafile %<cr>", options)
 
 -- deselect matching string after search
 vim.keymap.set('n', '<leader>.', "<cmd>noh<cr>", options)
@@ -34,7 +43,7 @@ vim.keymap.set('n', '<leader>gh', "<cmd><cr>", options)
 
 -- nerdcomenter
 vim.keymap.set('n', '<C-_>', "<Plug>NERDCommenterToggle", options)
-vim.keymap.set('n', '<C-_>', "<Plug>NERDCommenterToggle<CR>gv", options)
+vim.keymap.set('v', '<C-_>', "<Plug>NERDCommenterToggle<CR>gv", options)
 
 -- files
 -- open File Explorer in the current dir
