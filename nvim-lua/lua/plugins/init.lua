a2ddb3aded0a22b5f99119bdeccb98cddf952bdd
ssoteloserrano <ssoteloserrano@gmail.com>
2022-08-31 20:18:07 -0600

    Add lsp installer plugin

--- nvim-lua/lua/plugins/init.lua
+++ nvim-lua/lua/plugins/init.lua
@@ -1,37 +1,56 @@
 
 return require('packer').startup(function(use)
-    -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+	-- Packer can manage itself
+	use 'wbthomason/packer.nvim'
 
-    -- Colorschemes
-    use 'tiagovla/tokyodark.nvim'
-    use { "EdenEast/nightfox.nvim", tag = "v1.0.0" }
+	-- Colorschemes
+	use 'tiagovla/tokyodark.nvim'
+	use { "EdenEast/nightfox.nvim", tag = "v1.0.0" }
 
-    use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
+	use {'nvim-treesitter/nvim-treesitter', run = ":TSUpdate"}
 
-    use 'kyazdani42/nvim-web-devicons'
+	use 'kyazdani42/nvim-web-devicons'
 
-    use 'tiagofumo/vim-nerdtree-syntax-highlight'
+	use 'tiagofumo/vim-nerdtree-syntax-highlight'
 
-    -- Tabline & Statusline
-    use 'nvim-lualine/lualine.nvim'
-    use 'romgrk/barbar.nvim'
+	-- Tabline & Statusline
+	use 'nvim-lualine/lualine.nvim'
+	use 'romgrk/barbar.nvim'
 
-    use 'lukas-reineke/indent-blankline.nvim'
+	use 'lukas-reineke/indent-blankline.nvim'
 
-    use {'junegunn/fzf', run = "fzf#install()"}
+	use {'junegunn/fzf', run = "fzf#install()"}
 
-    -- General
-    use {
-	'kyazdani42/nvim-tree.lua',
-	requires = {
-	    'kyazdani42/nvim-web-devicons',
-	},
-    }
+	-- General
+	use 'kyazdani42/nvim-tree.lua'
+	
+	use 'mhinz/vim-startify'
 
-    use 'mhinz/vim-startify'
+	-- Code
+	use 'preservim/nerdcommenter'
 
-    -- Code
-    use 'preservim/nerdcommenter'
+	-- LSP
+	use {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"neovim/nvim-lspconfig",
+		"williamboman/nvim-lsp-installer",
+	}
+
+	-- Completion
+	use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
+	use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+	use 'hrsh7th/cmp-buffer'
+	use 'hrsh7th/cmp-path'
+	use 'hrsh7th/cmp-cmdline'
+	use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
+	use 'L3MON4D3/LuaSnip' -- Snippets plugin
+	use 'onsails/lspkind-nvim'
+	use 'ray-x/lsp_signature.nvim'
+
+	use {
+		"windwp/nvim-autopairs",
+		config = function() require("nvim-autopairs").setup {} end
+	}
 
 end)
