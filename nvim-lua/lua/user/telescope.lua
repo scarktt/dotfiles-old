793a605e6c6d1c17bad95a8fba0dbb1a19225e69
ssoteloserrano <ssoteloserrano@gmail.com>
2022-09-09 23:31:59 -0600

    Remove fzf from telescope

--- nvim-lua/lua/user/telescope.lua
+++ nvim-lua/lua/user/telescope.lua
@@ -42,17 +42,17 @@ telescope.setup {
     },
   },
   extensions = {
-    fzf = {
-      fuzzy = true, -- false will only do exact matching
-      override_generic_sorter = true, -- override the generic sorter
-      override_file_sorter = true, -- override the file sorter
-      case_mode = "smart_case", -- or "ignore_case" or "respect_case"
-      -- the default case_mode is "smart_case"
-    }
+    -- fzf = {
+    --   fuzzy = true, -- false will only do exact matching
+    --   override_generic_sorter = true, -- override the generic sorter
+    --   override_file_sorter = true, -- override the file sorter
+    --   case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+    --   -- the default case_mode is "smart_case"
+    -- }
   }
 }
 
-telescope.load_extension("fzf")
+-- telescope.load_extension("fzf")
 telescope.load_extension("projects")
 
 local opts = { noremap = true, silent = true }
